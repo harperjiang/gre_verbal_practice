c9a02765d3bb6bb3ffc689a7b0826458e82f9b77
Hao Jiang <harperjiang@msn.com>
2015-01-26 18:00:59 -0500

    Bug fixing

---
+++ GRE Verbal Practice/PercentChartView.h
@@ -0,0 +1,13 @@
+//
+//  PercentChartView.h
+//  GRE Verbal Master
+//
+//  Created by Harper on 1/24/15.
+//  Copyright (c) 2015 Hao Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PercentChartView : UIView
+
+@end
---
+++ GRE Verbal Practice/PercentChartView.m
@@ -0,0 +1,21 @@
+//
+//  PercentChartView.m
+//  GRE Verbal Master
+//
+//  Created by Harper on 1/24/15.
+//  Copyright (c) 2015 Hao Jiang. All rights reserved.
+//
+
+#import "PercentChartView.h"
+
+@implementation PercentChartView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
